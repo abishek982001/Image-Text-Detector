16bd1fea10df1072285a57c18f88e508de07ee03
abishek982001 <abishek982001@gmail.com>
2020-07-01 00:10:56 +0530

    Done character Detection

--- textDetector.py
+++ textDetector.py
@@ -6,14 +6,18 @@ image = cv2.imread('test.png')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # print(pytesseract.image_to_string(image))
 
-### Detecting Characters
-height_of_image, width_of_image,_ = image.shape
-boxes = pytesseract.image_to_boxes(image)
-for box_info in boxes.splitlines():
-    box_info = box_info.split(' ')
-    #print(box_info)
-    x,y,w,h = int(box_info[1]), int(box_info[2]), int(box_info[3]), int(box_info[4])  # Get the coordinates to draw boxes
-    cv2.rectangle(image, (x,height_of_image-y), (w,height_of_image-h), (0,0,255), 1)  # Draws the boxes
+def detect_characters():
+    """Function that detects text and draws boxes character by character"""
+    image_height, image_width,_ = image.shape
+    boxes = pytesseract.image_to_boxes(image)
+    for box_info in boxes.splitlines():
+        box_info = box_info.split(' ')
+        # print(box_info)
+        x,y,w,h = int(box_info[1]), int(box_info[2]), int(box_info[3]), int(box_info[4])  # Get the coordinates to draw boxes
+        cv2.rectangle(image, (x,image_height-y), (w,image_height-h), (0,0,255), 2)  # Draws the boxes
+        cv2.putText(image, box_info[0], (x, image_height-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
+
+detect_characters()
 cv2.imshow("Result", image)
 cv2.waitKey(0)

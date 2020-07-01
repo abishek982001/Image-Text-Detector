d3a7d8506ba5fd04344d03506ea2a5411437daf3
abishek982001 <abishek982001@gmail.com>
2020-07-01 12:25:07 +0530

    Added user inputs and handled errors

--- textDetector.py
+++ textDetector.py
@@ -1,12 +1,8 @@
 import cv2
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
-image = cv2.imread('test.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# print(pytesseract.image_to_string(image))
 
-def detect_characters():
+def detect_characters(putText=False):
     """Function that detects text and draws boxes character by character"""
     image_height, image_width,_ = image.shape
     boxes = pytesseract.image_to_boxes(image)
@@ -15,9 +11,10 @@ def detect_characters():
         # print(box_info)
         x,y,w,h = int(box_info[1]), int(box_info[2]), int(box_info[3]), int(box_info[4])  # Get the coordinates to draw boxes
         cv2.rectangle(image, (x,image_height-y), (w,image_height-h), (0,0,255), 2)  # Draws the boxes
-        cv2.putText(image, box_info[0], (x, image_height-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+        if putText == True:
+            cv2.putText(image, box_info[0], (x, image_height-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
-def detect_words():
+def detect_words(putText=False):
     """Function that detects words and draws boxes"""
     image_height, image_width,_ = image.shape
     boxes = pytesseract.image_to_data(image)
@@ -28,9 +25,10 @@ def detect_words():
             if len(box_info)==12:
                 x,y,w,h = int(box_info[6]), int(box_info[7]), int(box_info[8]), int(box_info[9])  # Get the coordinates to draw boxes
                 cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255), 3)
-                cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+                if putText == True:
+                    cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
-def detect_digits_only():
+def detect_digits_only(putDigit=False):
     """Function that detects digits only"""
     image_height, image_width,_ = image.shape
     confg = r'--oem 3 --psm 6 outputbase digits'  # Configure tesseract to detect digits only
@@ -42,9 +40,45 @@ def detect_digits_only():
             if len(box_info)==12:
                 x,y,w,h = int(box_info[6]), int(box_info[7]), int(box_info[8]), int(box_info[9])  # Get the coordinates to draw boxes
                 cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255), 3)
-                cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+                if putDigit == True:
+                    cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
-# detect_characters()
-detect_digits_only()
-cv2.imshow("Result", image)
-cv2.waitKey(0)
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
+
+while(True):
+
+    print("*****Menu*****")
+    print("1. Detect characters in image")
+    print("2. Detect words in image")
+    print("3. Quit")
+    choice = int(input("Enter your choice:"))
+    if choice > 3 or choice < 1:
+        print("Invlid choice")
+        continue
+    if choice == 3:
+        print("Quitting...")
+        exit(0)
+
+    try:
+        path = input("Enter the path of the image:")
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    except:
+        print("Invalid path")
+        continue
+    output_text = input("Do you want the detected text to be printed in the image Enter (yes/no)")
+    if output_text.lower() == 'no':
+        flag = False
+    elif output_text.lower() == 'yes':
+        flag = True
+    else:
+        print("Invalid input")
+        continue
+    if choice == 1:
+        detect_characters(flag)
+    if choice == 2:
+        detect_words(flag)
+
+    cv2.imshow("Result", image)
+    cv2.waitKey(0)

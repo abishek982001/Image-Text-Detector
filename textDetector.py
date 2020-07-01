ddfb9a35a1ee995bdde762e7ca349fa7ae0ea38f
abishek982001 <abishek982001@gmail.com>
2020-07-01 12:43:05 +0530

    Followed OOPS Guidance

--- textDetector.py
+++ textDetector.py
@@ -1,47 +1,48 @@
 import cv2
 import pytesseract
 
+class BoundingBox:
 
-def detect_characters(putText=False):
-    """Function that detects text and draws boxes character by character"""
-    image_height, image_width,_ = image.shape
-    boxes = pytesseract.image_to_boxes(image)
-    for box_info in boxes.splitlines():
-        box_info = box_info.split(' ')
-        # print(box_info)
-        x,y,w,h = int(box_info[1]), int(box_info[2]), int(box_info[3]), int(box_info[4])  # Get the coordinates to draw boxes
-        cv2.rectangle(image, (x,image_height-y), (w,image_height-h), (0,0,255), 2)  # Draws the boxes
-        if putText == True:
-            cv2.putText(image, box_info[0], (x, image_height-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
-
-def detect_words(putText=False):
-    """Function that detects words and draws boxes"""
-    image_height, image_width,_ = image.shape
-    boxes = pytesseract.image_to_data(image)
-    for count, box_info in enumerate(boxes.splitlines()):
-        if count!=0:
-            box_info = box_info.split()
+    def detect_characters(self, putText=False):
+        """Function that detects text and draws boxes character by character"""
+        image_height, image_width,_ = image.shape
+        boxes = pytesseract.image_to_boxes(image)
+        for box_info in boxes.splitlines():
+            box_info = box_info.split(' ')
             # print(box_info)
-            if len(box_info)==12:
-                x,y,w,h = int(box_info[6]), int(box_info[7]), int(box_info[8]), int(box_info[9])  # Get the coordinates to draw boxes
-                cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255), 3)
-                if putText == True:
-                    cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+            x,y,w,h = int(box_info[1]), int(box_info[2]), int(box_info[3]), int(box_info[4])  # Get the coordinates to draw boxes
+            cv2.rectangle(image, (x,image_height-y), (w,image_height-h), (0,0,255), 2)  # Draws the boxes
+            if putText == True:
+                cv2.putText(image, box_info[0], (x, image_height-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
-def detect_digits_only(putDigit=False):
-    """Function that detects digits only"""
-    image_height, image_width,_ = image.shape
-    confg = r'--oem 3 --psm 6 outputbase digits'  # Configure tesseract to detect digits only
-    boxes = pytesseract.image_to_data(image, config=confg)
-    for count, box_info in enumerate(boxes.splitlines()):
-        if count!=0:
-            box_info = box_info.split()
-            # print(box_info)
-            if len(box_info)==12:
-                x,y,w,h = int(box_info[6]), int(box_info[7]), int(box_info[8]), int(box_info[9])  # Get the coordinates to draw boxes
-                cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255), 3)
-                if putDigit == True:
-                    cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+    def detect_words(self, putText=False):
+        """Function that detects words and draws boxes"""
+        image_height, image_width,_ = image.shape
+        boxes = pytesseract.image_to_data(image)
+        for count, box_info in enumerate(boxes.splitlines()):
+            if count!=0:
+                box_info = box_info.split()
+                # print(box_info)
+                if len(box_info)==12:
+                    x,y,w,h = int(box_info[6]), int(box_info[7]), int(box_info[8]), int(box_info[9])  # Get the coordinates to draw boxes
+                    cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255), 3)
+                    if putText == True:
+                        cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+
+    def detect_digits_only(self, putDigit=False):
+        """Function that detects digits only"""
+        image_height, image_width,_ = image.shape
+        confg = r'--oem 3 --psm 6 outputbase digits'  # Configure tesseract to detect digits only
+        boxes = pytesseract.image_to_data(image, config=confg)
+        for count, box_info in enumerate(boxes.splitlines()):
+            if count!=0:
+                box_info = box_info.split()
+                # print(box_info)
+                if len(box_info)==12:
+                    x,y,w,h = int(box_info[6]), int(box_info[7]), int(box_info[8]), int(box_info[9])  # Get the coordinates to draw boxes
+                    cv2.rectangle(image, (x,y), (w+x,h+y), (0,0,255), 3)
+                    if putDigit == True:
+                        cv2.putText(image, box_info[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
@@ -76,9 +77,9 @@ while(True):
         print("Invalid input")
         continue
     if choice == 1:
-        detect_characters(flag)
+        BoundingBox.detect_characters(flag)
     if choice == 2:
-        detect_words(flag)
+        BoundingBox.detect_words(flag)
 
     cv2.imshow("Result", image)
     cv2.waitKey(0)
